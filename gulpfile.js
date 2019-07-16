ab19e93af12e16f3a98fedd6dd3923873e218c5c
Dawid <dawidkopicki@gmail.com>
2019-07-16 18:13:34 +0200

    Gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('sass', () => {
+    return gulp.src('./app/sass/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./app/sass/*.scss', gulp.series('sass'))
+});
+
+gulp.task('imagemin', () => {
+   gulp.src('./app/images/*.+(png|jpg)')
+       .pipe(imagemin())
+       .pipe(gulp.dest('./dist/images'))
+});
